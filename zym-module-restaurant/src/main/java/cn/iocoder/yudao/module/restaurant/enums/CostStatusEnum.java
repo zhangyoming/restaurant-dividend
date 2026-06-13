6986a06ab51d1d4b2f14442dea0ad150dc41b326
zhangyoming <18581952009@163.com>
2026-06-13 22:49:03 +0900

    成本支出状态枚举

---
+++ zym-module-restaurant/src/main/java/cn/iocoder/yudao/module/restaurant/enums/CostStatusEnum.java
@@ -0,0 +1,41 @@
+package cn.iocoder.yudao.module.restaurant.enums;
+
+import cn.iocoder.yudao.framework.common.core.ArrayValuable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+/**
+ * 成本支出状态枚举
+ *
+ * @author zhangyoming
+ */
+@Getter
+@AllArgsConstructor
+public enum CostStatusEnum implements ArrayValuable<Integer> {
+
+    WAIT_CONFIRM(0, "待确认"),
+    CONFIRMED(1, "已确认"),
+    CANCELED(2, "已作废");
+
+    public static final Integer[] ARRAYS = Arrays.stream(values())
+            .map(CostStatusEnum::getStatus)
+            .toArray(Integer[]::new);
+
+    /**
+     * 状态
+     */
+    private final Integer status;
+
+    /**
+     * 状态名
+     */
+    private final String name;
+
+    @Override
+    public Integer[] array() {
+        return ARRAYS;
+    }
+
+}
